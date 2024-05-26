7fb43050d1ddfa3efcc7e7283fe2ceb4ce0ae494
05abdulla <05abdulla2@gmail.com>
2024-05-26 18:11:22 +0300

    Add files via upload

---
+++ Proga metodi strok.py
@@ -0,0 +1,8 @@
+#my_string = input()
+#print(len(my_string))
+my_string = "Привет, меня зовут Абдула"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
